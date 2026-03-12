3b2228e24a2f626faad4c39701d161b60fd2cd65
Yareth Chavez <yguevara-chavez@csub.edu>
2026-03-12 13:52:01 -0700

    Refactor calculator with modular arithmetic functions
    Refactored calculator to use separate functions for operations and removed environment variable dependency.

--- calculator.c
+++ calculator.c
@@ -1,40 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
-int main() {
-    int a, b; 
-    char op; 
-    int result; 
-
-    char *query = getenv("QUERY_STRING");
-    if (query == NULL) {
-        printf("No input recived."); 
-        return 0;
-    }
-
-    sscanf(query, "a=%d&b=%d&op=%c", &a, &b, &op);
-
-    switch (op) {
-        case '+':
-            result = a + b; 
-            break;
-        case '-':
-            result = a - b; 
-            break;
-        case '*':
-            result = a * b; 
-            break;
-        case '/':
-            if (b == 0) {
-                printf("Division by zero is not allowed."); 
-                return 0;
-            }
-            result = a / b; 
-            break;
-        default:
-            printf("Invalid operator."); 
-            return 0;
-    }
-    return 0;
-
+double calc_add      (double a, double b) { 
+    return a + b;
 }
+
+double calc_subtract (double a, double b) { 
+    return a - b;
+}
+
+double calc_multiply (double a, double b) { 
+    return a * b;
+}
+
+double calc_divide  (double a, double b) { 
+    return a / b; 
+} 
+
+double calc_modulo  (double a, double b) { 
+    return fmod(a, b);
+}
+
+/* power operation (a^b) */
+double calc_power   (double a, double b) {
+    return pow(a, b);
+}
+
